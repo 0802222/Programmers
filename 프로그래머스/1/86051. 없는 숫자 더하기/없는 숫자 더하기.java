660e07e7363689b7f2d071c4dd9c7418ba8ab371
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-27 06:13:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for (int i = 0; i <= 9; i++) {
+            boolean exist = false;
+            
+            for (int num : numbers) {
+                if(num == i) {
+                    exist = true;
+                    break;
+                }
+            }
+            if(!exist) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}

fc1e2ff88248a030b864793075f183570c3bd3cc
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-29 05:06:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n) {
-        
-        // list 로 담고
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 1; i <= n; i++) {
-            if ( i % 2 == 1) {
-               list.add(i);                
-            }
-        }
-        
-        // list size 만큼 배열을 생성해서 담는다
-        int[] answer = new int[list.size()];
-        
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        int[] answer = {};
         return answer;
     }
 }

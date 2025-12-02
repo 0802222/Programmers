99c3451a14082d6aae78b07acf11f66e7d1990d7
이초롱 <132190391+0802222@users.noreply.github.com>
2025-12-02 17:52:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.09 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        return sb.toString();
+    }
+}

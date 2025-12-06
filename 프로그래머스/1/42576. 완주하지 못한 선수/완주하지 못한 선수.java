8137fd4b63c95d03b018bed75ede0fdf164d61f7
이초롱 <132190391+0802222@users.noreply.github.com>
2025-12-06 21:56:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.03 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < participant.length; i++) {
+            String name = participant[i];
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+           String name = completion[i];
+            map.put(name, map.get(name) - 1);
+        }
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                return entry.getKey();
+            }
+        }
+        return "";
+    }
+}
